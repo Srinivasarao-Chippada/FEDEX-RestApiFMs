bb43acf2c52d9e1c1e98e44701e3d05c4a37af55
Srini <Srinivasarao.Chippada@elemica.com>
2026-04-02 15:20:48 +0530

    Create trigger_rfc.py

---
+++ trigger_rfc.py
@@ -0,0 +1,17 @@
+from pyrfc import Connection
+import os
+
+conn = Connection(
+    user=os.environ['SAP_USER'],
+    passwd=os.environ['SAP_PASS'],
+    ashost=os.environ['SAP_HOST'],
+    sysnr=os.environ['SAP_SYSNR'],
+    client=os.environ['SAP_CLIENT']
+)
+
+result = conn.call('Z_TRIGGER_FEDEX')
+
+print(result)
+
+if result.get('STATUS') != 'SUCCESS':
+    raise Exception("RFC Execution Failed")
